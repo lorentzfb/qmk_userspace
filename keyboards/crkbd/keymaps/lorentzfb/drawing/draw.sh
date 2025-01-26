46fffee10997b354f55395d57e291cf522be8896
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2025-01-26 21:05:02 +0100

    update wf and draw script

--- keyboards/crkbd/keymaps/lorentzfb/drawing/draw.sh
+++ keyboards/crkbd/keymaps/lorentzfb/drawing/draw.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 # https://keymap-drawer.streamlit.app
 
-DIST_APP_SVG="/Users/lorentz/projects/personal/kbd/src/lib/assets/crkbd_rev1_lorentzfb_app.svg"
+[[ -n "$1" ]] && KEYMAP_DEV_OUT="$1"
 
 DIST="dist"
 CONFIG_PARSE="config_parse.yaml"
@@ -11,6 +10,7 @@ CONFIG_DRAW="config_draw.yaml"
 CONFIG_OUT="${DIST}/config.yaml"
 KEYMAP_JSON="${DIST}/keymap.json"
 KEYMAP_SVG="${DIST}/keymap.svg"
+KEYMAP_SVG_UNSTYLED="${DIST}/keymap_unstyled.svg"
 
 # Exit on any error
 set -e
@@ -33,5 +33,15 @@ cat "${CONFIG_DRAW}" >> "${CONFIG_OUT}"
 # Create keymap SVG
 keymap draw -o "${KEYMAP_SVG}" "${CONFIG_OUT}"
 
-# Copy SVG to app dir
-cp "${KEYMAP_SVG}" "${DIST_APP_SVG}"
+# Create keymap SVG with no styles
+CONFIG=$(cat "${CONFIG_OUT}")
+CONFIG+=$'
+  svg_style: |
+    /* I will handle it... */
+'
+echo "${CONFIG}" | keymap draw -o "${KEYMAP_SVG_UNSTYLED}" -
+
+# Copy dev SVG to dev app dir
+[[ -n "$KEYMAP_DEV_OUT" ]] || exit 0
+cp "${KEYMAP_SVG_UNSTYLED}" "${KEYMAP_DEV_OUT}"
+echo "Wrote dev keymap to ${KEYMAP_DEV_OUT}"
